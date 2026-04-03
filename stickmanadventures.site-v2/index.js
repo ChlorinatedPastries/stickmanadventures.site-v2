f2ed6d0e5575881fcce67a30e376b506006aa386
ChlorinatedPastries <ChlorinatedPastriesXD@proton.me>
2026-04-03 13:37:40 -0500

    back

---
+++ stickmanadventures.site-v2/index.js
@@ -0,0 +1,21 @@
+const randomThing = ["3.014 Vigintillion Cookies.","THE FACTORY MUST GROW","Welcome to the Information Superhighway!","NOWS YOUR CHANCE TO BE A [[BIG SHOT]]"];
+
+function main() {
+    getRandomSentence();
+    title();
+}
+
+function getRandomSentence() {  
+    let randomIndex = Math.floor(Math.random() * randomThing.length);
+    let result = randomThing[randomIndex];
+    return result;
+}
+
+function title() {
+    let randomIndex = Math.floor(Math.random() * randomThing.length);
+    let result = randomThing[randomIndex];
+    document.getElementById("title").innerHTML = result;
+    return result;
+}
+
+main();
